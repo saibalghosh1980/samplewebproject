1ad3018b858fafaaf0e3caaf1d5f48fdee6186cf
Saibal Kumar Ghosh <saibal.ghosh.1980@gmail.com>
2021-08-17 12:12:45 +0100

    Initial commit

---
+++ src/main/java/com/oup/samplewebproject/SamplewebprojectApplication.java
@@ -0,0 +1,13 @@
+package com.oup.samplewebproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplewebprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplewebprojectApplication.class, args);
+	}
+
+}
